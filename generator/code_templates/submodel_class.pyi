a9ffaf6a566e43f3d541db0fcad80722131a3746
zrgt <garmaev@gmx.de>
2023-05-08 16:18:31 +0200

    Handle submodel_elements in templates
    Passed se arguments can be not only of Type SubmodelElement, but also be empty or can be Iterable e.g. list of properties passed into a collection

--- generator/code_templates/submodel_class.pyi
+++ generator/code_templates/submodel_class.pyi
@@ -21,10 +21,19 @@
 {% endblock %}
 
 {%- block in_init -%}
-submodel_elements = [{% for se in submodel_elements_args -%} {{ se }}{% if not loop.last %},{% endif %}{%- endfor %}]
+embedded_submodel_elements = []
+for se_arg in [{% for se in submodel_elements_args -%} {{ se }}{% if not loop.last %},{% endif %}{%- endfor %}]:
+    if se_arg is None:
+        continue
+    elif isinstance(se_arg, SubmodelElement):
+        embedded_submodel_elements.append(se_arg)
+    elif isinstance(se_arg, Iterable):
+        embedded_submodel_elements.extend(se_arg)
+    else:
+        raise TypeError(f"Unknown type of value in submodel_element_args: {type(se_arg)}")
 {% endblock %}
 
 {%- block init_super_args -%}
-submodel_element=[se for se in submodel_elements if isinstance(se, SubmodelElement)],
+submodel_element= embedded_submodel_elements,
 {{ super() }}
 {% endblock %}
